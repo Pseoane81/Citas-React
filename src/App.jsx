574770c2952388dc97f57e7e4886d37d686d749f
Pablo Seoane <pablitoseoane@gmail.com>
2022-08-16 16:56:10 -0300

    Agregando Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header"
+import Formulario from "./components/Formulario"
+import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
   return (
-    <>
+    <div className="conteiner mx-auto mt-20">
       <Header/>
-    </>
+      <Formulario/>
+      <ListadoPacientes/>
+    </div>
   )
 }
 
