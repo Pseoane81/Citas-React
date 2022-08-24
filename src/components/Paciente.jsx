3c75e612ce07fc5478e32706732c686dfd514e57
Pablo Seoane <pablitoseoane@gmail.com>
2022-08-24 16:57:31 -0300

    Pacientes

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,33 @@
+const Paciente = () => {
+  return (
+            <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+                <p className="font-bold mb-3 text-gray-700 uppercase">
+                    Nombre:{" "}
+                    <span className="font-normal normal-case">Hook</span>
+                </p>
+
+                <p className="font-bold mb-3 text-gray-700 uppercase">
+                    Propietario:{" "}
+                    <span className="font-normal normal-case">Pablo Seoane</span>
+                </p>
+
+                <p className="font-bold mb-3 text-gray-700 uppercase">
+                    Email:{" "}
+                    <span className="font-normal normal-case">Correo@correo.com</span>
+                </p>
+
+                <p className="font-bold mb-3 text-gray-700 uppercase">
+                    Fecha Alta:{" "}
+                    <span className="font-normal normal-case">10 de Diciembre de 2022</span> 
+                </p>
+
+                <p className="font-bold mb-3 text-gray-700 uppercase">
+                    Sintomas:{" "}
+                    <span className="font-normal normal-case">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi magni inventore non architecto error repudiandae sed qui, odit corporis accusantium vero saepe, quisquam iure aperiam, vel labore porro laborum numquam.</span>
+                </p>
+
+            </div>
+  )
+}
+
+export default Paciente
